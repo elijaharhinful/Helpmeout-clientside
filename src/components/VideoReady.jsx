0fa3140038c2010770d9d17f26afde41a1a65275
elijaharhinful <elijaharhinful8@gmail.com>
2023-10-02 18:55:30 +0000

    added modal to save button

--- src/components/VideoReady.jsx
+++ src/components/VideoReady.jsx
@@ -1,10 +1,22 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
+
+import { AnimatePresence } from 'framer-motion';
+import VideoModal from './VideoModal';
 import VideoReadyMain from "./VideoReadyMain";
 
 
 function VideoReady() {
+    const [modalIsOpen, setIsOpen] = React.useState(false);
+    function openModal() {
+        setIsOpen(true);
+    }
+
+    function closeModal() {
+        setIsOpen(false);
+    }
+
     return (
         <div className="ready__container">
             <Header />
@@ -13,8 +25,12 @@ function VideoReady() {
                 <h5>To ensure the availability and privacy of your video,
                     <br></br>
                     we recommend saving it to your account.</h5>
-                <a href="#" className="button button--small">Save Video</a>
+                <a className="button button--small" onClick={openModal}>Save Video</a>
                 <p>Don’t have an account? <a href="#">Create account</a></p>
+
+                <AnimatePresence>
+                    {modalIsOpen && <VideoModal isOpen={modalIsOpen} closeModal={closeModal} />}
+                </AnimatePresence>
             </div>
             <Footer />
         </div>
